9891ebf56f59ee70babf54b8b7adca44828e282b
Trabajo <Trabajo@192.168.1.35>
2017-06-04 14:39:00 +0200

    Modificamos el archivo

--- banco/src/banco/Banco.java
+++ banco/src/banco/Banco.java
@@ -16,6 +16,8 @@ public class Banco {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Modificación del archivo java");
     }
     
 }
